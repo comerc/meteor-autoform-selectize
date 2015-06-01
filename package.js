b0db4e96913c92b901ad170611dfbe997430463b
comerc <comerc@mail.ru>
2015-06-02 01:18:16 +0300

    support autoform 5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'comerc:autoform-selectize',
   summary: 'Custom "selectize" input type for AutoForm',
-  version: '2.2.2',
+  version: '2.2.3',
   git: 'https://github.com/comerc/meteor-autoform-selectize.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0');
   api.addFiles([
     'autoform-selectize.html',
     'autoform-selectize.js',
